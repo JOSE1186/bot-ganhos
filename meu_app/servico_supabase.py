8bf826791c364f4ca570c2c2b1831755f142e7f2
Jose Arthur <arthurbarros971@gmail.com>
2025-08-22 22:33:20 -0300

    Update servico_supabase.py

--- meu_app/servico_supabase.py
+++ meu_app/servico_supabase.py
@@ -1,16 +1,5 @@
 from supabase import create_client
-import os
 
-class SupabaseService:
-    def __init__(self):
-        self.url = os.getenv("SUPABASE_URL")
-        self.key = os.getenv("SUPABASE_KEY")
-        self.client = create_client(self.url, self.key)
-
-    def salvar_dados(self, numero, ganhos, combustivel, liquido):
-        self.client.table("ganhos").insert({
-            "numero_usuario": numero,
-            "ganhos": ganhos,
-            "combustivel": combustivel,
-            "liquido": liquido
-        }).execute()
+url = "https://mbyuhxjbwmvbhpieywjm.supabase.co"
+key = "SUA_CHAVE_AQUI"
+supabase = create_client(url, key)
