664943a769007d4dced54103b344932140657365
Jose Arthur <arthurbarros971@gmail.com>
2025-08-22 22:34:15 -0300

    Update servico_twilio.py

--- meu_app/servico_twilio.py
+++ meu_app/servico_twilio.py
@@ -1,17 +1,1 @@
-from twilio.rest import Client
-import os
-
-class TwilioService:
-    def __init__(self):
-        self.sid = os.getenv("TWILIO_SID")
-        self.token = os.getenv("TWILIO_TOKEN")
-        self.numero_envio = os.getenv("TWILIO_NUMBER")
-        self.client = Client(self.sid, self.token)
-
-    def enviar_resposta(self, numero, liquido):
-        mensagem = f"Seu ganho líquido foi R$ {liquido:.2f}"
-        self.client.messages.create(
-            to=numero,
-            from_=self.numero_envio,
-            body=mensagem
-        )
+from twilio.twiml.messaging_response import MessagingResponse
