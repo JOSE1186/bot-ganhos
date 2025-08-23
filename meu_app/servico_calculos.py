d0facb4fb672a55eef9c2bb16f7fd5dd810e2e45
Jose Arthur <arthurbarros971@gmail.com>
2025-08-22 22:32:51 -0300

    Update servico_calculos.py

--- meu_app/servico_calculos.py
+++ meu_app/servico_calculos.py
@@ -1,12 +1,6 @@
-class CalculosService:
-    def extrair_dados(self, mensagem):
-        try:
-            partes = mensagem.split()
-            ganhos = float(partes[0])
-            combustivel = float(partes[1])
-            return ganhos, combustivel
-        except:
-            return 0, 0
-
-    def calcular_liquido(self, ganhos, combustivel):
-        return ganhos - combustivel
+def tentar_converter_para_float(texto):
+    try:
+        texto_limpo = texto.strip().replace(",", ".")
+        return float(texto_limpo)
+    except:
+        return None
